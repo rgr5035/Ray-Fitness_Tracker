a0d05853fda1e0f6eda23fa6ae39a3574900da5f
Robby Ray <rgr5035@gmail.com>
2021-02-05 11:57:23 -0700

    working on routes and seed data population

---
+++ models/workout.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema ({
+
+    day: {
+        type: Date,
+        default: Date.now
+    },
+
+    exercises: [
+        {
+
+        type: {
+            type: String, 
+            trim: true,
+            required: "Please enter resistance or cardio"
+            },
+
+        name: {
+            type: String, 
+            trim: true,
+            required: "Please enter a workout name"
+        },
+
+        duration: {
+            type: Number, 
+            required: true
+        },
+
+        weight: {
+            type: Number,
+            required: true  
+        },
+
+        sets: {
+            type: Number, 
+            required: true
+        },
+
+        reps: {
+            type: Number, 
+            required: true
+        },
+        }]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema)
+
+module.exports = Workout;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
 
+
+//1 post, 1 put, 2 get, 1 delete
+
 router.post("/api/workout", ({ body }, res) => {
-    
+
 })
+
+
+
+module.exports = router;
