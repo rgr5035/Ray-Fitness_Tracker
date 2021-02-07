8c5c52172171754af10f6e707f8c261fdd5a4141
Robby Ray <rgr5035@gmail.com>
2021-02-07 12:08:28 -0700

    added code for deploy

--- server.js
+++ server.js
@@ -14,9 +14,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
   {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
   }
 );
